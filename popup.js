53aa5a2066a34357504fe9926d63369247d98285
Milos Pavicic <milospavicic@yahoo.com>
2018-11-01 09:42:01 +0100

    ContextMenu added

--- popup.js
+++ popup.js
@@ -2,8 +2,37 @@ window.onload=function(){
     document.getElementById("music-bookmark").addEventListener("click", musicBookmark);
     document.getElementById("bar-bookmark").addEventListener("click", basicBookmark);
     
+	chrome.contextMenus.removeAll(function (callback){
+		chrome.contextMenus.create({
+			 title: "Add to bar",
+			 contexts:["page"],  // ContextType
+			 id:"bar",
+			 onclick: basicBookmark // A callback function
+			});
+			chrome.contextMenus.create({	
+			 title: "Add to Music",
+			 contexts:["page"],   // ContextType
+			 id:"music",
+			 onclick: musicBookmark // A callback function
+		});
+	});
+	chrome.contextMenus.onClicked.addListener(function(itemData) {
+	  if (itemData.menuItemId == "bar")
+		basicBookmark();
+	  if (itemData.menuItemId == "music")
+		musicBookmark();
+});
+	
 } 
  
+function musicBookmark() {
+    var id = "1";
+    chrome.tabs.getSelected(null,function(tab) { // null defaults to current window
+        var title = tab.title;
+        var url = tab.url;
+        createTestBookmark(id, title, url);
+    });
+}
 function basicBookmark() {
     var id = "1";
     chrome.tabs.getSelected(null,function(tab) { // null defaults to current window
@@ -12,20 +41,11 @@ function basicBookmark() {
         createTestBookmark(id, title, url);
     });
 }
-function musicBookmark() {
-    var id = "5";
-    chrome.tabs.getSelected(null,function(tab) { // null defaults to current window
-        var title = tab.title;
-        var url = tab.url;
-        createTestBookmark(id, title, url);
-    });
-}
+
 function createTestBookmark(parentId, title, url) {
-    chrome.bookmarks.create({'parentId': parentId,'title': title,'url': url},
-        function(newBookmark) {
-            console.log("added bookmark: " + newBookmark.title);
-    });
-}
+    chrome.bookmarks.create({'parentId': parentId,'title': title,'url': url})
+};
+
 
 //var map = {}; 
 //onkeydown = onkeyup = function(e){
